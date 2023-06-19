f11e201d3973bf22853cc59c9316a8efe17c2511
Zynad <zynadlol@gmail.com>
2023-06-19 22:36:25 +0200

    Added ef and db

---
+++ WebAPI/Contexts/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models.Entities;
+
+namespace WebAPI.Contexts;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+    public DbSet<AppUser> Users { get; set; }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
